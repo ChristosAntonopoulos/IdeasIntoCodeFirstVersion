46fab5b1fa634c0469db4ac2accd483ff25b4460
Filippos Kontoes <fil13kon@gmail.com>
2020-04-30 17:19:14 +0300

    NewBranch

---
+++ IdeasIntoCodeFirstVersion/Migrations/202004301412525_NewDataBase.cs
@@ -0,0 +1,16 @@
+namespace IdeasIntoCodeFirstVersion.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NewDataBase : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
